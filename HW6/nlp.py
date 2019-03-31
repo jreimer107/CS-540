148f94e53d4170fd9a44e4e4cc4020bb1391fc7c
John Reimer <jreimer107@gmail.com>
2019-03-31 14:05:18 -0500

    rc graphing done

--- HW6/nlp.py
+++ HW6/nlp.py
@@ -1,5 +1,7 @@
+"""HW6"""
 import collections
 import os
+import matplotlib.pyplot as plt
 
 # Tally occurrences of words in a file
 ctr = collections.Counter()
@@ -8,6 +10,32 @@ for filename in os.listdir('news'):
         for word in file.read().split():
             ctr[word] += 1
 
-numWords = sum(ctr.values())
-print(numWords)
+NUMWORDS = sum(ctr.values())
+print(NUMWORDS)
 print(ctr.most_common(20))
+
+rc_data = []
+ordered_keys = []
+i = 0
+for word in ctr.most_common():
+    ordered_keys.append(word[0])
+    rc_data.append(word[1])
+    i += 1
+
+# RC plot
+# Shows that there are only a few words that are used many times,
+# the majority of words is only used a few times.
+fig, ax = plt.subplots()
+ax.plot(ordered_keys, rc_data)
+ax.get_xaxis().set_visible(False)
+plt.savefig('rc.png')
+
+# LogR LogC plot
+# Shows that the ratio between rank and count is, for the most part,
+# the same across all words
+fig, ax = plt.subplots()
+ax.set_yscale('log')
+ax.set_xscale('log')
+ax.plot(ordered_keys, rc_data)
+ax.get_xaxis().set_visible(False)
+plt.savefig('logrlogc.png')
