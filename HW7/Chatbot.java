a0e532909960f7f05dec008f5c8ae1c29178e7bd
John Reimer <jreimer107@gmail.com>
2019-04-07 21:28:30 -0500

    Untracked .out files. Good up till 500.

--- HW7/Chatbot.java
+++ HW7/Chatbot.java
@@ -4,147 +4,183 @@ import java.io.*;
 import java.text.DecimalFormat;
 
 public class Chatbot {
-    private static String filename = "./corpus.txt";
+	private static String filename = "./corpus.txt";
 
-    private static ArrayList<Integer> readCorpus() {
-        ArrayList<Integer> corpus = new ArrayList<Integer>();
-        try {
-            File f = new File(filename);
-            Scanner sc = new Scanner(f);
-            while (sc.hasNext()) {
-                if (sc.hasNextInt()) {
-                    int i = sc.nextInt();
-                    corpus.add(i);
-                } else {
-                    sc.next();
-                }
-            }
-        } catch (FileNotFoundException ex) {
-            System.out.println("File Not Found.");
-        }
-        return corpus;
-    }
+	private static ArrayList<Integer> readCorpus() {
+		ArrayList<Integer> corpus = new ArrayList<Integer>();
+		try {
+			File f = new File(filename);
+			Scanner sc = new Scanner(f);
+			while (sc.hasNext()) {
+				if (sc.hasNextInt()) {
+					int i = sc.nextInt();
+					corpus.add(i);
+				} else {
+					sc.next();
+				}
+			}
+		} catch (FileNotFoundException ex) {
+			System.out.println("File Not Found.");
+		}
+		return corpus;
+	}
 
-    static public void main(String[] args) {
-        ArrayList<Integer> corpus = readCorpus();
-        int flag = Integer.valueOf(args[0]);
+	static public void main(String[] args) {
+		ArrayList<Integer> corpus = readCorpus();
+		int flag = Integer.valueOf(args[0]);
 
-        int v = 4700;
-        DecimalFormat df = new DecimalFormat("#.#######");
+		int v = 4700;
+		DecimalFormat df = new DecimalFormat("0.0000000");
 
-        int counts[] = new int[v];
-        double probs[] = new double[v];
+		if (flag == 100) {
+			int w = Integer.valueOf(args[1]);
+			int count = 0;
+			for (int token : corpus) {
+				if (token == w) {
+					count++;
+				}
+			}
+			double prob = (count + 1) / (double) (corpus.size() + v);
 
-        for (int i = 0; i < corpus.size(); i++) {
-            counts[corpus.get(i)]++;
-        }
-        for (int i = 0; i < v; i++) {
-            probs[i] = (counts[i] + 1) / (double) (corpus.size() + v);
-        }
-        if (flag == 100) {
-            int w = Integer.valueOf(args[1]);
-            // int count = 0;
-            // for (int token : corpus) {
-            // if (token == w) {
-            // count++;
-            // }
-            // }
+			System.out.println(count);
+			System.out.println(df.format(prob));
 
-            
-            System.out.println(counts[w]);
-            System.out.println(df.format(probs[w]));
+		} else if (flag == 200) {
+			int n1 = Integer.valueOf(args[1]);
+			int n2 = Integer.valueOf(args[2]);
 
-        } else if (flag == 200) {
-            int n1 = Integer.valueOf(args[1]);
-            int n2 = Integer.valueOf(args[2]);
+			double r = n1 / (double) n2;
 
-            double r = n1 / (double) n2;
+			int counts[] = new int[v];
+			double probs[] = new double[v];
 
-            // Get li
-            double intervalSize = 1 / (double) v;
-            System.out.println(intervalSize);
-            int interval = (int) (r / intervalSize);
+			for (int i = 0; i < corpus.size(); i++) {
+				counts[corpus.get(i)]++;
+			}
+			for (int i = 0; i < v; i++) {
+				probs[i] = (counts[i] + 1) / (double) (corpus.size() + v);
+			}
 
-            double li = 0;
-            double ri = 0;
-            for (int j = 0; j < interval; j++) {
-                li += probs[j];
-            }
+			// Get li and ri
+			double li = 0;
+			double ri = probs[0];
+			int j = 1;
+			while (ri < r && j < v) {
+				li = ri;
+				ri += probs[j++];
+			}
 
-            // ri is just the one extra probability
-            ri = li + probs[interval];
+			System.out.println(j - 1);
+			System.out.println(df.format(li));
+			System.out.println(df.format(ri));
 
-            System.out.println(interval);
-            System.out.println(df.format(li));
-            System.out.println(df.format(ri));
+		} else if (flag == 300) {
+			int h = Integer.valueOf(args[1]);
+			int w = Integer.valueOf(args[2]);
+			int hcount = 0;
+			int hwcount = 0;
+			for (int i = 0; i < corpus.size() - 1; i++) {
+				if (corpus.get(i) == h) {
+					hcount++;
+					if (corpus.get(i + 1) == w) {
+						hwcount++;
+					}
+				}
+			}
+			double prob = (hwcount + 1) / (double) (hcount + v);
 
-        } else if (flag == 300) {
-            int h = Integer.valueOf(args[1]);
-            int w = Integer.valueOf(args[2]);
-            int count = 0;
-            ArrayList<Integer> words_after_h = new ArrayList<Integer>();
-            // TODO
+			System.out.println(hwcount);
+			System.out.println(hcount);
+			System.out.println(df.format(prob));
 
-        } else if (flag == 400) {
-            int n1 = Integer.valueOf(args[1]);
-            int n2 = Integer.valueOf(args[2]);
-            int h = Integer.valueOf(args[3]);
-            // TODO
+		} else if (flag == 400) {
+			int n1 = Integer.valueOf(args[1]);
+			int n2 = Integer.valueOf(args[2]);
+			int h = Integer.valueOf(args[3]);
 
-        } else if (flag == 500) {
-            int h1 = Integer.valueOf(args[1]);
-            int h2 = Integer.valueOf(args[2]);
-            int w = Integer.valueOf(args[3]);
-            int count = 0;
-            ArrayList<Integer> words_after_h1h2 = new ArrayList<Integer>();
-            // TODO
+			double r = n1 / (double) n2;
 
-        } else if (flag == 600) {
-            int n1 = Integer.valueOf(args[1]);
-            int n2 = Integer.valueOf(args[2]);
-            int h1 = Integer.valueOf(args[3]);
-            int h2 = Integer.valueOf(args[4]);
-            // TODO
-        } else if (flag == 700) {
-            int seed = Integer.valueOf(args[1]);
-            int t = Integer.valueOf(args[2]);
-            int h1 = 0, h2 = 0;
+			int counts[] = new int[v];
+			double probs[] = new double[v];
 
-            Random rng = new Random();
-            if (seed != -1)
-                rng.setSeed(seed);
+			int hcount = 0;
+			for (int i = 0; i < corpus.size() - 1; i++) {
+				if (corpus.get(i) == h) {
+					hcount++;
+					counts[corpus.get(i + 1)]++;
+				}
+			}
+			for (int i = 0; i < v; i++) {
+				probs[i] = (counts[i] + 1) / (double) (hcount + v);
+			}
 
-            if (t == 0) {
-                // TODO Generate first word using r
-                double r = rng.nextDouble();
-                System.out.println(h1);
-                if (h1 == 9 || h1 == 10 || h1 == 12) {
-                    return;
-                }
+			// Get li and ri
+			double li = 0;
+			double ri = probs[0];
+			int j = 1;
+			while (ri < r && j < v) {
+				li = ri;
+				ri += probs[j++];
+			}
 
-                // TODO Generate second word using r
-                r = rng.nextDouble();
-                System.out.println(h2);
-            } else if (t == 1) {
-                h1 = Integer.valueOf(args[3]);
-                // TODO Generate second word using r
-                double r = rng.nextDouble();
-                System.out.println(h2);
-            } else if (t == 2) {
-                h1 = Integer.valueOf(args[3]);
-                h2 = Integer.valueOf(args[4]);
-            }
+			System.out.println(j - 1);
+			System.out.println(df.format(li));
+			System.out.println(df.format(ri));
 
-            while (h2 != 9 && h2 != 10 && h2 != 12) {
-                double r = rng.nextDouble();
-                int w = 0;
-                // TODO Generate new word using h1,h2
-                System.out.println(w);
-                h1 = h2;
-                h2 = w;
-            }
-        }
+		} else if (flag == 500) {
+			int h1 = Integer.valueOf(args[1]);
+			int h2 = Integer.valueOf(args[2]);
+			int w = Integer.valueOf(args[3]);
+			int count = 0;
+			ArrayList<Integer> words_after_h1h2 = new ArrayList<Integer>();
+			// TODO
 
-        return;
-    }
+		} else if (flag == 600) {
+			int n1 = Integer.valueOf(args[1]);
+			int n2 = Integer.valueOf(args[2]);
+			int h1 = Integer.valueOf(args[3]);
+			int h2 = Integer.valueOf(args[4]);
+			// TODO
+		} else if (flag == 700) {
+			int seed = Integer.valueOf(args[1]);
+			int t = Integer.valueOf(args[2]);
+			int h1 = 0, h2 = 0;
+
+			Random rng = new Random();
+			if (seed != -1)
+				rng.setSeed(seed);
+
+			if (t == 0) {
+				// TODO Generate first word using r
+				double r = rng.nextDouble();
+				System.out.println(h1);
+				if (h1 == 9 || h1 == 10 || h1 == 12) {
+					return;
+				}
+
+				// TODO Generate second word using r
+				r = rng.nextDouble();
+				System.out.println(h2);
+			} else if (t == 1) {
+				h1 = Integer.valueOf(args[3]);
+				// TODO Generate second word using r
+				double r = rng.nextDouble();
+				System.out.println(h2);
+			} else if (t == 2) {
+				h1 = Integer.valueOf(args[3]);
+				h2 = Integer.valueOf(args[4]);
+			}
+
+			while (h2 != 9 && h2 != 10 && h2 != 12) {
+				double r = rng.nextDouble();
+				int w = 0;
+				// TODO Generate new word using h1,h2
+				System.out.println(w);
+				h1 = h2;
+				h2 = w;
+			}
+		}
+
+		return;
+	}
 }
--- HW7/run_tests.sh
+++ HW7/run_tests.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-stopAt="$1"
+
+shopt -s expand_aliases
+source ~/.bashrc
+
+alias java="java.exe"
+alias javac="javac.exe"
+alias python="python.exe"
+
+doOnly="$1"
 
 javac Chatbot.java
 
@@ -8,12 +16,20 @@ for testcase in $testcases; do
     if [ "${testcase: -4}" == ".exp" ]; then
         testcase="$(echo "$testcase" | cut -f 1 -d '.')"
         args=$(echo "$testcase" | tr _ \ )
-        if [ ! -z $stopAt ]; then
-            if [[ $(echo $args | awk '{print $1}') -gt $stopAt ]]; then
-                break
+        if [ ! -z $doOnly ]; then
+            if [[ $(echo $args | awk '{print $1}') -lt $doOnly ]]; then
+                continue
             fi
         fi
         java Chatbot $args > testcases/$testcase.out
         echo "did $args"
+		if [ ! -z $doOnly ]; then
+            if [[ $(echo $args | awk '{print $1}') -gt $doOnly ]]; then
+                break
+            fi
+        fi
     fi
 done
+
+cd testcases
+python run_tests.py $doOnly
