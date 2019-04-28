6f79b8cd0239c897bc1d3a7e28e1056a59bd05a9
John Reimer <jreimer107@gmail.com>
2019-04-28 17:29:12 -0500

    first attempt at layer objects

--- HW10/NeuralNet.java
+++ HW10/NeuralNet.java
@@ -1,52 +1,57 @@
 import java.text.DecimalFormat;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.ArrayList;
+import java.util.*;
 
 class NeuralNet {
 
-	static Perceptron[] input_layer = new Perceptron[5];
-	static Perceptron[] hidden_layer = new Perceptron[3];
-	static Perceptron output_layer = new Perceptron();
+	// static Perceptron[] input_layer = new Perceptron[5];
+	// static Perceptron[] hidden_layer = new Perceptron[3];
+	// static Perceptron output_layer = new Perceptron();
+
+	static Layer input_layer = new Layer(4);
+	static Layer hidden_layer = new Layer(2);
+	static Layer output_layer = new Layer(1);
 
 	public static void main(String[] args) {
-		for (int i = 0; i < input_layer.length; i++)
-			input_layer[i] = new Perceptron();
-		for (int i = 0; i < hidden_layer.length; i++)
-			hidden_layer[i] = new Perceptron();
-
-		input_layer[0].SetActivation(1);
-		hidden_layer[0].SetActivation(1);
-
 		int flag = Integer.parseInt(args[0]);
 		DecimalFormat df = new DecimalFormat("0.00000");
 
 		if (flag == 100) {
 			// Arg parsing
 			int arg_index = 1;
-			for (int i = 1; i < hidden_layer.length; i++) {
-				for (Perceptron input : input_layer) {
-					input.SetWeight(hidden_layer[i], Double.parseDouble(args[arg_index++]));
+			for (Perceptron hidden : hidden_layer) {
+				for (Perceptron input : input_layer.All()) {
+					input.SetWeight(hidden, Double.parseDouble(args[arg_index++]));
 				}
 			}
-			for (Perceptron hidden : hidden_layer) {
-				hidden.SetWeight(output_layer, Double.parseDouble(args[arg_index++]));
+			for (Perceptron hidden : hidden_layer.All()) {
+				hidden.SetWeight(output_layer.get(0), Double.parseDouble(args[arg_index++]));
 			}
-			for (int i = 1; i < input_layer.length; i++) {
-				input_layer[i].SetActivation(Double.parseDouble(args[arg_index++]));
+			for (Perceptron input : input_layer) {
+				input.SetActivation(Double.parseDouble(args[arg_index++]));
 			}
+
+			// for (int i = 1; i < hidden_layer.length; i++) {
+			// for (Perceptron input : input_layer) {
+			// input.SetWeight(hidden_layer[i], Double.parseDouble(args[arg_index++]));
+			// }
+			// }
+			// for (Perceptron hidden : hidden_layer) {
+			// hidden.SetWeight(output_layer, Double.parseDouble(args[arg_index++]));
+			// }
+			// for (int i = 1; i < input_layer.length; i++) {
+			// input_layer[i].SetActivation(Double.parseDouble(args[arg_index++]));
+			// }
 
 			// Activation calculation
-			ArrayList<String> hidden_activations = new ArrayList<String>(hidden_layer.length);
+			input_layer.UpdateActivations();
+			hidden_layer.UpdateActivations();
+			output_layer.UpdateActivations();
+			ArrayList<String> hidden_activations = new ArrayList<String>(hidden_layer.size());
 			for (Perceptron hidden : hidden_layer) {
-				hidden.UpdateActivation();
 				hidden_activations.add(df.format(hidden.GetActivation()));
 			}
-			output_layer.UpdateActivation();
-
 			System.out.println(String.join(" ", hidden_activations));
-			System.out.println(df.format(output_layer.GetActivation()));
+			System.out.println(df.format(output_layer.get(0).GetActivation()));
 		}
 	}
 
@@ -83,7 +88,8 @@ class Perceptron {
 		for (Perceptron input : inputs) {
 			newActivation += input.GetActivation() * input.GetWeight(this);
 		}
-		this.SetActivation(NeuralNet.sigmoid(newActivation));
+		// this.SetActivation(NeuralNet.sigmoid(newActivation));
+		this.SetActivation(newActivation);
 	}
 
 	public void SetActivation(double a) {
@@ -101,21 +107,25 @@ class Perceptron {
 }
 
 class Layer implements Iterable<Perceptron> {
-	private HashSet<Perceptron> nodes;
+	private ArrayList<Perceptron> nodes;
 	private Perceptron bias;
 
 	public Layer(int size) {
-		nodes = new HashSet<Perceptron>(size);
+		nodes = new ArrayList<Perceptron>(size);
 		for (int i = 0; i < size; i++)
 			nodes.add(new Perceptron());
 		bias = new Perceptron();
 		bias.SetActivation(1.0);
 	}
 
-	public HashSet<Perceptron> GetAll() {
-		HashSet<Perceptron> all = new HashSet<Perceptron>(this.nodes);
+	public ArrayList<Perceptron> All() {
+		ArrayList<Perceptron> all = new ArrayList<Perceptron>(this.nodes);
 		all.add(this.bias);
 		return all;
+	}
+
+	public Perceptron get(int index) {
+		return nodes.get(index);
 	}
 
 	public Iterator<Perceptron> iterator() {
@@ -130,5 +140,9 @@ class Layer implements Iterable<Perceptron> {
 
 	public void SetBias(double newBias) {
 		this.bias.SetActivation(newBias);
+	}
+
+	public int size() {
+		return this.nodes.size();
 	}
 }
